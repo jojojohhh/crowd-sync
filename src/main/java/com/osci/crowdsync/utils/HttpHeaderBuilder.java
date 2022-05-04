3c9c7752c721d09aceafa28ef234630716d0dd4c
jojojohhh <qwhtkdwp@gmail.com>
2022-05-04 16:38:36 +0900

    HttpHeader.class Builder

--- src/main/java/com/osci/crowdsync/utils/HttpHeaderBuilder.java
+++ src/main/java/com/osci/crowdsync/utils/HttpHeaderBuilder.java
@@ -1,7 +1,5 @@
 package com.osci.crowdsync.utils;
 
-import lombok.Builder;
-import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
