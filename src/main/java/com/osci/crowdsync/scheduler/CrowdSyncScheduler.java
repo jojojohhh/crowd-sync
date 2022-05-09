cb4a95cfd5c778bb3b52a3c5452e341926c9f511
jojojohhh <qwhtkdwp@gmail.com>
2022-05-09 09:50:33 +0900

    scheduler

--- src/main/java/com/osci/crowdsync/scheduler/CrowdSyncScheduler.java
+++ src/main/java/com/osci/crowdsync/scheduler/CrowdSyncScheduler.java
@@ -1,7 +1,12 @@
 package com.osci.crowdsync.scheduler;
 
+import com.osci.crowdsync.dto.CrowdUserDto;
+import com.osci.crowdsync.dto.SysUserDto;
 import com.osci.crowdsync.service.CrowdUserService;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 @RequiredArgsConstructor
 public class CrowdSyncScheduler {
@@ -9,7 +14,8 @@ public class CrowdSyncScheduler {
     private final CrowdUserService crowdUserService;
 
     public void crowdSyncJob() {
-        //crowdUserService
+        Stream<SysUserDto> users = crowdUserService.findAllUsers();
+
     }
 
 }
