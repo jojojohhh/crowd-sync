f9fa2e7c34de9d04592b7ea246c09b8562ae90a3
jojojohhh <qwhtkdwp@gmail.com>
2022-05-12 09:05:21 +0900

    주석 추가

--- src/main/java/com/osci/crowdsync/config/properties/AtlassianProperties.java
+++ src/main/java/com/osci/crowdsync/config/properties/AtlassianProperties.java
@@ -6,6 +6,10 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 
+/**
+ * atlassian property binding class
+ * 모든 변수 또는 하위 property 는 상수로 선언
+ */
 @Getter
 @RequiredArgsConstructor
 @ConstructorBinding
